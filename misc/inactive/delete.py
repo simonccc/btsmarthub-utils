80a7b24cdd874fd0888999d41840d58464a38c36
simonccc <simonc@gmail.com>
2020-02-17 19:12:00 +0000

    unworking attempt to remove a device via cli

---
+++ misc/inactive/delete.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3 
+
+import time
+import requests
+import sys
+import hashlib
+import urllib.parse
+sys.path[0:0] = ['../../']
+import config as cfg
+
+login_body = ('O=helpdesk.htm&usr=admin&pws=' + hashlib.md5(cfg.hub['pass'].encode('utf-8')).hexdigest())
+
+# login to router
+def login():
+  print('logging in...')
+  login = requests.post(cfg.hub['url'] + '/login.cgi', cookies=cfg.cookies, data=login_body, allow_redirects=False)
+  time.sleep(1)
+
+def getpi():
+  print('getting pi')
+  pi = requests.get(cfg.hub['url'] + '/cgi/renewPi.js', cookies=cfg.cookies)
+  return(pi.text)
+
+login()
+pi = getpi()
+#print(pi)
+
+delstring = ('CMD=&GO=my_network.htm&SET0='+ '50426637' + '%3Dd%252C' + 'DE%2533A49%2533AAE%2533AA9%2533A3D%2533A5F' + '%253B' + '&pi=' + pi)
+
+#delstring2 = (urllib.parse.quote('CMD=&GO=my_network.htm&SET0=50426637=d,DE:49:AE:A9:3D:5F;' + '&pi=' + pi))
+#print(delstring)
+#print(delstring2)
+
+
+
+delete = requests.post(cfg.hub['url'] + '/apply.cgi', cookies=cfg.cookies,  data=(delstring))
+print(delete.request.headers)
+print(delete.request.body)
+print (delete.status_code)
+print (delete.text)
---
+++ misc/inactive/list-in-active.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3 
+
+import requests
+import urllib.parse
+import re
+import sys
+sys.path[0:0] = ['../../']
+import config as cfg
+
+# request page
+r = requests.get(cfg.hub['url']+ '/cgi/cgi_basicMyDevice.js')
+
+def print_c(color, string):
+  if cfg.tail_colors['enabled'] == 'true':
+   return(cfg.tail_colors[color] + string  + '\x1b[0m ')
+  else:
+   return(string)
+
+content = r.content
+mac_c = 1
+vars = content.decode().split("\n")
+
+for var in vars:
+#    print(var)
+    var_types = var.split(",")
+
+    # fix for the first mac address
+    if (re.search('var known_device_list', var_types[0])):
+       var_types[0] = var_types[0].replace('var known_device_list=[', '')
+    var_types[0] = var_types[0].replace('{', '')
+    var_types[0] = var_types[0].replace('\'', '')
+
+    try:
+      mac = urllib.parse.unquote(var_types[0].split(":")[1])
+      rmac = var_types[0].split(":")[1]
+    except:
+     continue 
+
+    try:
+      macf = urllib.parse.unquote(var_types[0].split(":")[0])
+      if (re.search('mac', macf)):
+        pass
+      else:
+        continue
+    except:
+     pass 
+
+    try: 
+      hostname = urllib.parse.unquote(var_types[1].split(":")[1]).replace('\'', '')
+    except:
+     continue
+    try:
+      name = urllib.parse.unquote(var_types[4].split(":")[1]).replace('\'', '')
+    except:
+      continue
+    try: 
+      os = urllib.parse.unquote(var_types[6].split(":")[1]).replace('\'', '')
+    except:
+      continue
+    device = urllib.parse.unquote(var_types[7].split(":")[1]).replace('\'', '')
+    if (hostname == '' ):
+        hostname = name
+    if (re.search('unknown_', hostname)):
+        hostname = os
+    if (re.search('Unknown', hostname)):
+        hostname = device
+    ip = urllib.parse.unquote(var_types[2].split(":")[1]).replace('\'', '')
+    activity = urllib.parse.unquote(var_types[5].split(":")[1])
+    mac_c = (int(mac_c) + 1)
+
+    if (activity == '\'1\'' ):
+       continue
+    os = urllib.parse.unquote(var_types[6].split(":")[1])
+    active = urllib.parse.unquote(var_types[9].split(":")[1]).replace('\'', '')
+    port = urllib.parse.unquote(var_types[11].split(":")[1]).replace('\'', '')
+
+    print(str(mac_c) + ' ' + print_c('blue', mac),rmac,ip, print_c('yellow',hostname),print_c('green',port),active)
+
