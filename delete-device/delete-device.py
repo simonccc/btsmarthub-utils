8bf3f1b21e1266f9e7fd3eed9c41a33b9fc752ee
simonccc <simonc@gmail.com>
2020-06-13 19:45:29 +0100

    style fixes

--- delete-device/delete-device.py
+++ delete-device/delete-device.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import datetime, re, requests, hashlib, sys
 
 # check for mac
@@ -15,22 +14,26 @@ sys.path[0:0] = ['../']
 import config as cfg
 
 # login
-login_body = ('O=helpdesk.htm&usr=admin&pws=' + hashlib.md5(cfg.hub['pass'].encode('utf-8')).hexdigest())
-login = requests.post(cfg.hub['url'] + '/login.cgi', cookies=cfg.cookies, data=login_body)
+lb = ('O=helpdesk.htm&usr=admin&pws=' + hashlib.md5(cfg.hub['pass'].encode('utf-8')).hexdigest())
+requests.post(cfg.hub['url'] + '/login.cgi', cookies=cfg.cookies, data=lb)
 
 # get pi and current ts
 pi = requests.get(cfg.hub['url'] + '/cgi/renewPi.js', cookies=cfg.cookies).text
 ts = int(datetime.datetime.now().strftime('%s%f')[:-3])
 
 # get cfg id for updating known devices list
-n = requests.get(cfg.hub['url'] + '/cgi/cgi_myNetwork.js?ts=' + str(ts)).text
+n = requests.get(cfg.hub['url'] + '/cgi/cgi_myNetwork.js?ts=' + str(ts), cookies=cfg.cookies).text
 for line in n.splitlines():
     if re.search('known_devices_update', line):
         cfg_id = line.split(',')[1]
 
-# prepare the post body and header
-delstring = ('CMD=&GO=my_network.htm&SET0='+ cfg_id + '%3Dd%252C' + mac + '%253B' + '&pi=' + pi)
+# generate the delete string
+ds = ('CMD=&GO=my_network.htm&SET0='+ cfg_id + '%3Dd%252C' + mac + '%253B' + '&pi=' + pi)
 
 # send delete request
-delete = requests.post(cfg.hub['url'] + '/apply.cgi', data=(delstring), cookies=cfg.cookies)
-print (delete.status_code)
+dr = requests.post(cfg.hub['url'] + '/apply.cgi', data=(ds), cookies=cfg.cookies)
+if (dr.status_code != 200):
+    print ('ERROR: ', dr.status_code)
+    print (dr.request.body)
+    exit(1)
+print('OK')
