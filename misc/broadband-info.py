142b2a7541b367bc9e19c74f3c2f9fef6cb95a95
simonccc <simonc@gmail.com>
2021-05-28 19:51:42 +0100

    broadband info script now useful

--- misc/broadband-info.py
+++ misc/broadband-info.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 import requests, re, sys
 import urllib.parse
@@ -19,7 +19,7 @@ def print_c(color, string):
   else:
    return(string)
 
-# request status page 
+# request status page
 r = requests.get(cfg.hub['url'] + '/cgi/cgi_broadband.js?', cookies=cfg.cookies, allow_redirects=False)
 
 # if a 302 login
@@ -29,9 +29,31 @@ if (str(r.status_code) == '302'):
 
 content = r.content
 vars = content.decode().split(";")
+
 count=0
 
-# split the event log var 
+# https://stackoverflow.com/questions/12523586/python-format-size-application-converting-b-to-kb-mb-gb-tb
+# changed to 1000 for the smarthub
+def humanbytes(B):
+   'Return the given bytes as a human friendly KB, MB, GB, or TB string'
+   B = float(B)
+   KB = float(1000)
+   MB = float(KB ** 2) # 1,048,576
+   GB = float(KB ** 3) # 1,073,741,824
+   TB = float(KB ** 4) # 1,099,511,627,776
+
+   if B < KB:
+      return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
+   elif KB <= B < MB:
+      return '{0:.2f} KB'.format(B/KB)
+   elif MB <= B < GB:
+      return '{0:.2f} MB'.format(B/MB)
+   elif GB <= B < TB:
+      return '{0:.2f} GB'.format(B/GB)
+   elif TB <= B:
+      return '{0:.2f} TB'.format(B/TB)
+
+# split the event log var
 for var in vars:
   var_o = var.strip('\r\n')
   var_o = str(var_o.replace('\n', ''))
@@ -44,7 +66,15 @@ for var in vars:
   if (count > 10):
     break
 
-  if (re.search('internet', stat) and (str(value) == '1')):
-    print('broadband is', print_c('green', 'ONLINE'))
   if (re.search('wan_link_rate_list', stat)):
-    print('broadband speed', print_c('green', var_o))
+    link = (var_o.split("'")[3]).replace("%3B", ";").split(";")
+    upload = humanbytes(int(link[0]))
+    download = humanbytes(int(link[1]))
+    print("rate:  " + download + " down " + upload + " up")
+
+  if (re.search('wan_conn_volume_list', stat)):
+    bw = (var_o.split("'")[3]).replace("%3B", ";")
+    total = humanbytes(int(bw.split(";")[0]))
+    download = humanbytes(int(bw.split(";")[1]))
+    upload = humanbytes(int(bw.split(";")[2]))
+    print("usage: " + total + " total (" + download + " down " + upload +" up)")
